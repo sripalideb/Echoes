7e6f4929aa30e199a03e01bbe94046361e16c9db
Sripali <sripali.deb.official@gmail.com>
2025-10-21 00:40:25 +0530

    Change base path from '/echoes/' to '/Echoes/'

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: '/echoes/',
+  base: '/Echoes/',
   plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
